ee6ae8c64d98c0c275b6bcc52b4ca6550fa12f8c
bugpanda <463847160@qq.com>
2017-10-06 16:16:02 +0800

    initial

---
+++ yuubari/src/main/java/ohana/yuubari/YuubariApplication.java
@@ -0,0 +1,12 @@
+package ohana.yuubari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuubariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuubariApplication.class, args);
+	}
+}
